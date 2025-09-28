384f2e0ccd491623d3064c20023873047e75a220
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-28 11:55:19 +0000

    style(global): import Google Fonts in global.css for Tailwind setup

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,19 +1,9 @@
-import Layout from "@/components/layouts/Layout"; 
-import Button from "@/components/common/Button";
-
-export default function Home() {
+const Home = () => {
   return (
-    <Layout>
-      <div className="flex flex-col items-center justify-center min-h-screen p-6">
-        <h1 className="text-3xl font-bold mb-6">Welcome to ALX Project 0x03</h1>
-        <Button 
-          buttonLabel="Click Me"
-          buttonBackgroundColor="green"
-          buttonSize="text-lg"
-          action={() => alert("Button clicked!")}
-/>
-
-      </div>
-    </Layout>
-  );
+   <div>
+    <h1> Welcome to Splash App </h1>
+   </div>
+  )
 }
+
+export default Home;
